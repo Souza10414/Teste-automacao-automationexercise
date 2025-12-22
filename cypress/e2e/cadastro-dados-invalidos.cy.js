0522f2cd21e96740110eb7132e2eb68da25bf70b
Nathanael Henrique Souza Nunes <nathanhenrique.nunes.souza@gmail.com>
2025-12-21 21:30:23 -0300

    chore: adiciona index do Mochawesome

--- cypress/e2e/cadastro-dados-invalidos.cy.js
+++ cypress/e2e/cadastro-dados-invalidos.cy.js
@@ -1,54 +1,51 @@
-import { faker } from '@faker-js/faker'
+import { faker } from "@faker-js/faker";
+import "cypress-mochawesome-reporter/register";
 
-describe('Cadastro - Criação de conta com dados inválidos (bug)', () => {
+describe("Cadastro - Criação de conta com dados inválidos (bug)", () => {
+  it("Deve criar conta e exibir mensagem de sucesso", () => {
+    // Página de signup
+    cy.visit("/signup");
 
-    it('Deve criar conta e exibir mensagem de sucesso', () => {
+    cy.get('[data-qa="signup-name"]').type(faker.person.fullName());
+    cy.get('[data-qa="signup-email"]').type(faker.internet.email());
+    cy.get('[data-qa="signup-button"]').click();
 
-        // Página de signup
-        cy.visit('/signup')
+    // Dados da conta
+    cy.get("#id_gender1").check(); // Mr
+    cy.get("#password").type("123456");
 
-        cy.get('[data-qa="signup-name"]').type(faker.person.fullName())
-        cy.get('[data-qa="signup-email"]').type(faker.internet.email())
-        cy.get('[data-qa="signup-button"]').click()
+    cy.get("#days").select("10");
+    cy.get("#months").select("May");
+    cy.get("#years").select("1995");
 
-        // Dados da conta
-        cy.get('#id_gender1').check() // Mr
-        cy.get('#password').type('123456')
+    // Dados pessoais
+    cy.get("#first_name").type(faker.person.firstName());
+    cy.get("#last_name").type(faker.person.lastName());
 
-        cy.get('#days').select('10')
-        cy.get('#months').select('May')
-        cy.get('#years').select('1995')
+    // Endereço (com dados inválidos propositalmente)
+    cy.get("#address1").type(faker.lorem.words(5));
+    cy.get("#country").select("United States");
+    cy.get("#state").type(faker.lorem.words(5));
+    cy.get("#city").type(faker.lorem.words(5));
+    cy.get("#zipcode").type(faker.lorem.words(5)); // inválido (BUG)
+    cy.get("#mobile_number").type(faker.lorem.words(5)); // inválido (BUG)
 
-        // Dados pessoais
-        cy.get('#first_name').type(faker.person.firstName())
-        cy.get('#last_name').type(faker.person.lastName())
+    // Criar conta
+    cy.get('[data-qa="create-account"]').click();
 
-        // Endereço (com dados inválidos propositalmente)
-        cy.get('#address1').type(faker.lorem.words(5))
-        cy.get('#country').select('United States')
-        cy.get('#state').type(faker.lorem.words(5))
-        cy.get('#city').type(faker.lorem.words(5))
-        cy.get('#zipcode').type(faker.lorem.words(5)) // inválido (BUG)
-        cy.get('#mobile_number').type(faker.lorem.words(5)) // inválido (BUG)
+    // Valida sucesso
+    cy.url().should("include", "/account_created");
 
-        // Criar conta
-        cy.get('[data-qa="create-account"]').click()
+    cy.contains(
+      "Congratulations! Your new account has been successfully created!"
+    ).should("not.be.visible");
 
-        // Valida sucesso
-        cy.url().should('include', '/account_created')
+    cy.contains(
+      "You can now take advantage of member privileges to enhance your online shopping experience with us."
+    ).should("not.be.visible");
 
-        cy.contains(
-            'Congratulations! Your new account has been successfully created!'
-        ).should('not.be.visible')
-
-        cy.contains(
-            'You can now take advantage of member privileges to enhance your online shopping experience with us.'
-        ).should('not.be.visible')
-
-        cy.get('[data-qa="continue-button"]')
-            .should('not.be.visible')
-            .and('contain.text', 'Continue')
-
-    })
-
-})
+    cy.get('[data-qa="continue-button"]')
+      .should("not.be.visible")
+      .and("contain.text", "Continue");
+  });
+});
--- cypress/e2e/valida-url-tela-inicial.cy.js
+++ cypress/e2e/valida-url-tela-inicial.cy.js
@@ -3,7 +3,7 @@ import "cypress-mochawesome-reporter/register";
 describe("Validação da página home", () => {
   it("Deve validar a URL e confirmar carregamento da página", () => {
     //Acessar o site
-    cy.visit("https://www.automationexercise.com/");
+    cy.visit("/");
 
     //Validar URL
     cy.url().should("eq", "https://www.automationexercise.com/");
